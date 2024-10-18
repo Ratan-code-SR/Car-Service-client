6d5fabbdf0cc39d6362f92503157fcd2840986eb
ratan chakma <ratanchakma68@gmail.com>
2024-10-18 20:26:30 +0600

    env file added

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -3,9 +3,7 @@ import Home from '../pages/Home/Home/Home.jsx';
 import { createBrowserRouter } from "react-router-dom";
 import Register from '../pages/Register/Register.jsx';
 import Login from '../pages/Login/Login.jsx';
-import Error from '../pages/Error/Error.jsx'
 import Checkout from '../pages/Checkout/Checkout.jsx';
-import BookService from '../pages/BookService/BookService.jsx';
 import Bookings from '../pages/Bookings/Bookings.jsx';
 import PrivateRoute from '../components/protected/PrivateRoute.jsx';
 
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDnHQhW8XSAaNn7JX1V8tySp4fkhHT3SRU",
-    authDomain: "car-service-client-a4480.firebaseapp.com",
-    projectId: "car-service-client-a4480",
-    storageBucket: "car-service-client-a4480.appspot.com",
-    messagingSenderId: "481917200838",
-    appId: "1:481917200838:web:4b45bc6182fed901642ac7"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
